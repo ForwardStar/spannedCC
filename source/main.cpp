0fe4261189225f7a6a0b18be7064cc7537af9214
Xie Haoxuan <119010351@link.cuhk.edu.cn>
2022-12-30 07:16:47 +0000

    Add support multiple query files

--- source/main.cpp
+++ source/main.cpp
@@ -25,24 +25,25 @@ int main(int argc, char * argv[]) {
     unsigned long long start_time = currentTime();
 
     if (argc != 5) {
-        std::cout << "Parameter error. Please check the readme file." << std::endl;
-    	return 0;
+        std::cout << "Parameters are non-standard. Please check the readme file." << std::endl;
     }
 
     TemporalGraph * Graph = build(argv);
     Graph->shrink_to_fit();
     int vertex_num = Graph->numOfVertices();
 
-    if (std::strcmp(argv[4], "Online") == 0) {
-        std::cout << "Running online search..." << std::endl;
-        unsigned long long online_search_start_time = currentTime();
-        online(Graph, argv[2], argv[3]);
-        unsigned long long online_search_end_time = currentTime();
-        std::cout << "Online search completed in " << timeFormatting(online_search_end_time - online_search_start_time).str() << std::endl;
+    if (std::strcmp(argv[argc - 1], "Online") == 0) {
+        for (int i = 2; i < argc - 2; i++) {
+            std::cout << "Running online search..." << std::endl;
+            unsigned long long online_search_start_time = currentTime();
+            online(Graph, argv[i], argv[argc - 2]);
+            unsigned long long online_search_end_time = currentTime();
+            std::cout << "Online search completed in " << timeFormatting(online_search_end_time - online_search_start_time).str() << std::endl;
+        }
         delete Graph;
     }
 
-    if (std::strcmp(argv[4], "Baseline") == 0) {
+    if (std::strcmp(argv[argc - 1], "Baseline") == 0) {
         std::cout << "Running baseline..." << std::endl;
         std::cout << "Constructing the index structure..." << std::endl;
         unsigned long long index_construction_start_time = currentTime();
@@ -51,47 +52,49 @@ int main(int argc, char * argv[]) {
         std::cout << "Index construction completed in " << timeFormatting(difftime(index_construction_end_time, index_construction_start_time)).str() << std::endl;
         std::cout << "Index cost " << Index->size() << " bytes" << std::endl;
         delete Graph;
-        std::cout << "Solving queries..." << std::endl;
-        unsigned long long query_start_time = currentTime();
-        baseline(Index, vertex_num, argv[2], argv[3]);
-        unsigned long long query_end_time = currentTime();
-        std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+        for (int i = 2; i < argc - 2; i++) {
+            std::cout << "Solving queries..." << std::endl;
+            unsigned long long query_start_time = currentTime();
+            baseline(Index, vertex_num, argv[i], argv[argc - 2]);
+            unsigned long long query_end_time = currentTime();
+            std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+        }
         std::cout << "Baseline completed!" << std::endl;
     }
 
-    if (std::strcmp(argv[4], "OBaseline") == 0) {
-        std::cout << "Running optimized baseline..." << std::endl;
-        std::cout << "Constructing the index structure..." << std::endl;
-        unsigned long long index_construction_start_time = currentTime();
-        OptimizedBaseline *Index = new OptimizedBaseline(Graph);
-        unsigned long long index_construction_end_time = currentTime();
-        std::cout << "Index construction completed in " << timeFormatting(difftime(index_construction_end_time, index_construction_start_time)).str() << std::endl;
-        delete Graph;
-        std::cout << "Solving queries..." << std::endl;
-        unsigned long long query_start_time = currentTime();
-        optimized_baseline(Index, vertex_num, argv[2], argv[3]);
-        unsigned long long query_end_time = currentTime();
-        std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
-        std::cout << "Optimized Baseline completed!" << std::endl;
-    }
+    // if (std::strcmp(argv[4], "OBaseline") == 0) {
+    //     std::cout << "Running optimized baseline..." << std::endl;
+    //     std::cout << "Constructing the index structure..." << std::endl;
+    //     unsigned long long index_construction_start_time = currentTime();
+    //     OptimizedBaseline *Index = new OptimizedBaseline(Graph);
+    //     unsigned long long index_construction_end_time = currentTime();
+    //     std::cout << "Index construction completed in " << timeFormatting(difftime(index_construction_end_time, index_construction_start_time)).str() << std::endl;
+    //     delete Graph;
+    //     std::cout << "Solving queries..." << std::endl;
+    //     unsigned long long query_start_time = currentTime();
+    //     optimized_baseline(Index, vertex_num, argv[2], argv[3]);
+    //     unsigned long long query_end_time = currentTime();
+    //     std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+    //     std::cout << "Optimized Baseline completed!" << std::endl;
+    // }
 
-    if (std::strcmp(argv[4], "Kruskal") == 0) {
-        std::cout << "Running kruskal reconstruction tree..." << std::endl;
-        std::cout << "Constructing the index structure..." << std::endl;
-        unsigned long long index_construction_start_time = currentTime();
-        KruskalReconstructionTree *Index = new KruskalReconstructionTree(Graph);
-        unsigned long long index_construction_end_time = currentTime();
-        std::cout << "Index construction completed in " << timeFormatting(difftime(index_construction_end_time, index_construction_start_time)).str() << std::endl;
-        delete Graph;
-        std::cout << "Solving queries..." << std::endl;
-        unsigned long long query_start_time = currentTime();
-        kruskal(Index, vertex_num, argv[2], argv[3]);
-        unsigned long long query_end_time = currentTime();
-        std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
-        std::cout << "Kruskal reconstruction tree completed!" << std::endl;
-    }
+    // if (std::strcmp(argv[4], "Kruskal") == 0) {
+    //     std::cout << "Running kruskal reconstruction tree..." << std::endl;
+    //     std::cout << "Constructing the index structure..." << std::endl;
+    //     unsigned long long index_construction_start_time = currentTime();
+    //     KruskalReconstructionTree *Index = new KruskalReconstructionTree(Graph);
+    //     unsigned long long index_construction_end_time = currentTime();
+    //     std::cout << "Index construction completed in " << timeFormatting(difftime(index_construction_end_time, index_construction_start_time)).str() << std::endl;
+    //     delete Graph;
+    //     std::cout << "Solving queries..." << std::endl;
+    //     unsigned long long query_start_time = currentTime();
+    //     kruskal(Index, vertex_num, argv[2], argv[3]);
+    //     unsigned long long query_end_time = currentTime();
+    //     std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+    //     std::cout << "Kruskal reconstruction tree completed!" << std::endl;
+    // }
 
-    if (std::strcmp(argv[4], "TSF") == 0) {
+    if (std::strcmp(argv[argc - 1], "TSF") == 0) {
         std::cout << "Running TSF-index..." << std::endl;
         std::cout << "Constructing the index structure..." << std::endl;
         unsigned long long index_construction_start_time = currentTime();
@@ -100,11 +103,13 @@ int main(int argc, char * argv[]) {
         std::cout << "Index construction completed in " << timeFormatting(difftime(index_construction_end_time, index_construction_start_time)).str() << std::endl;
         std::cout << "Index cost " << Index->size() << " bytes" << std::endl;
         delete Graph;
-        std::cout << "Solving queries..." << std::endl;
-        unsigned long long query_start_time = currentTime();
-        differential_kruskal(Index, vertex_num, argv[2], argv[3]);
-        unsigned long long query_end_time = currentTime();
-        std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+        for (int i = 2; i < argc - 2; i++) {
+            std::cout << "Solving queries..." << std::endl;
+            unsigned long long query_start_time = currentTime();
+            differential_kruskal(Index, vertex_num, argv[i], argv[argc - 2]);
+            unsigned long long query_end_time = currentTime();
+            std::cout << "Query completed in " << timeFormatting(query_end_time - query_start_time).str() << std::endl;
+        }
         std::cout << "TSF-index completed!" << std::endl;
     }
 
